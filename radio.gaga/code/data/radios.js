35ba4c975746df3bd2a909812c487e635231a2b3
KMG0908 <sdm6067@naver.com>
2019-10-25 19:00:27 +0900

    date 수정

--- radio.gaga/code/data/radios.js
+++ radio.gaga/code/data/radios.js
@@ -1,22 +1,53 @@
 module.exports = [
   {
-    title: '악동뮤지션 수현의 볼륨을 높여요',
-    dj: '이수현',
-    startTime: '20:00',
-    endTime: '22:00',
+    title: "악동뮤지션 수현의 볼륨을 높여요",
+    dj:"이수현",
+    startTime: "20:00",
+    endTime: "22:00",
     hz: 89.1,
-    channel: 'KBS2FM',
-    img: 'https://search.pstatic.net/common?type=o&size=190x150&quality=90&direct=true&src=http%3A%2F%2Fsstatic.naver.net%2Fkeypage%2Fimage%2Fdss%2F57%2F22%2F10%2F40%2F57_8221040_width_image_1528065312304.jpg',
-    site: 'http://program.kbs.co.kr/2fm/radio/svolume/'
+    channel: "KBS2FM",
+    img: "https://search.pstatic.net/common?type=o&size=190x150&quality=90&direct=true&src=http%3A%2F%2Fsstatic.naver.net%2Fkeypage%2Fimage%2Fdss%2F57%2F22%2F10%2F40%2F57_8221040_width_image_1528065312304.jpg",
+    site: "http://program.kbs.co.kr/2fm/radio/svolume/"
   },
   {
-    title: '배철수의 음악캠프',
-    dj: '배철수',
-    startTime: '18:00',
-    endTime: '20:00',
+    title: "배철수의 음악캠프",
+    dj:"배철수",
+    startTime: "18:00",
+    endTime: "20:00",
     hz: 91.9,
-    channel: 'MBCFM4U',
-    img: 'https://search.pstatic.net/common?type=o&size=190x150&quality=90&direct=true&src=http%3A%2F%2Fsstatic.naver.net%2Fkeypage%2Fimage%2Fdss%2F57%2F66%2F04%2F14%2F57_660414_width_image_1501743931419.jpg',
-    site: 'http://www.imbc.com/broad/radio/fm4u/musiccamp/'
+    channel: "MBCFM4U",
+    img: "https://search.pstatic.net/common?type=o&size=190x150&quality=90&direct=true&src=http%3A%2F%2Fsstatic.naver.net%2Fkeypage%2Fimage%2Fdss%2F57%2F66%2F04%2F14%2F57_660414_width_image_1501743931419.jpg",
+    site: "http://www.imbc.com/broad/radio/fm4u/musiccamp/"
+  },
+    {
+    title: "박소현의 러브게임",
+    dj:"박소현",
+    startTime: "18:00",
+    endTime: "20:00",
+    hz: 107.7,
+    channel: "SBSPOWERFM",
+    img: "https://search.pstatic.net/common?type=o&size=190x150&quality=90&direct=true&src=http%3A%2F%2Fsstatic.naver.net%2Fkeypage%2Fimage%2Fdss%2F57%2F66%2F04%2F49%2F57_660449_width_image_1428981645443.jpg",
+    site: "https://programs.sbs.co.kr/radio/lovegame"
+  },
+    {
+    title: "정오의 희망곡 김신영입니다",
+    dj:"김신영",
+    startTime: "12:00",
+    endTime: "14:00",
+    hz: 91.9,
+    channel: "MBCFM4U",
+    img: "https://search.pstatic.net/common?type=o&size=190x150&quality=90&direct=true&src=http%3A%2F%2Fsstatic.naver.net%2Fkeypage%2Fimage%2Fdss%2F57%2F67%2F21%2F07%2F57_672107_width_image_1418786052271.jpg",
+    site: "http://www.imbc.com/broad/radio/fm4u/musicparty/"
+  },
+  {
+    title: "김영철의 파워FM",
+    dj:"김영철",
+    startTime: "07:00",
+    endTime: "09:00",
+    hz: 107.7,
+    channel: "SBS파워FM",
+    img: "https://search.pstatic.net/common?type=o&size=190x150&quality=90&direct=true&src=http%3A%2F%2Fsstatic.naver.net%2Fkeypage%2Fimage%2Fdss%2F57%2F14%2F51%2F54%2F57_4145154_width_image_1499415096843.jpg",
+    site: "https://programs.sbs.co.kr/radio/0chulpowerfm"
   }
-]
+    
+ ]
